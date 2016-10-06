dc924810e5c2f460a52e8427c4a5b7f70712bcde
Stephen Hess <stephen.k.hess@gmail.com>
2016-10-06 16:45:09 -0400

    finished rest of initial import

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var tape = require('tape');
+var diff = require('difflet')({ indent : 2, comment : true });
+
+var common = {
+  // a visual deep diff rendered using console.error()
+  diff: function( actual, expected ){
+    console.error( diff.compare( actual, expected ) );
+  }
+};
+
+var tests = [
+  require ('./labelGenerator_AUS'),
+  require ('./labelGenerator_CAN'),
+  require ('./labelGenerator_default'),
+  require ('./labelGenerator_examples'),
+  require ('./labelGenerator_GBR'),
+  require ('./labelGenerator_USA'),
+  require ('./labelSchema')
+];
+
+tests.map(function(t) {
+  t.all(tape, common);
+});
